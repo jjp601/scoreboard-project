0248f9ba9c8791490f731d04a45570f08031a56c
Jon Powell <jpowell68601@gmail.com>
2018-01-31 21:27:41 -0500

    Initial Commit

---
+++ src/components/Counter.js
@@ -0,0 +1,23 @@
+import React, { PropTypes } from 'react';
+
+const Counter = (props) => {
+    return (
+      <div className="counter" >
+        <button className="counter-action decrement" onClick={() => props.updatePlayerScore(props.index, -1) }>
+          -
+        </button>
+        <div className="counter-score"> {props.score} </div>
+        <button className="counter-action increment" onClick={() => props.updatePlayerScore(props.index, 1) }>
+          +
+        </button>
+      </div>
+    );
+   }
+   
+Counter.propTypes = {
+    updatePlayerScore: React.PropTypes.func.isRequired,
+    index: React.PropTypes.number.isRequired,
+    score: React.PropTypes.number.isRequired,
+};
+
+export default Counter;
